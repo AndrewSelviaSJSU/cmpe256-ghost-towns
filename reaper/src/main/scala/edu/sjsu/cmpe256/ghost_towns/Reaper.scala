0cc80a9fc77c445a83534e77dce8b8e9f0ad8324
Andrew Selvia <andrew.selvia@sjsu.edu>
2020-05-04 02:34:32 -0700

    Update Reaper to handle larger files

--- reaper/src/main/scala/edu/sjsu/cmpe256/ghost_towns/Reaper.scala
+++ reaper/src/main/scala/edu/sjsu/cmpe256/ghost_towns/Reaper.scala
@@ -1,12 +1,13 @@
 package edu.sjsu.cmpe256.ghost_towns
 
+import java.nio.file.StandardOpenOption.{APPEND, CREATE, CREATE_NEW, TRUNCATE_EXISTING}
+import java.nio.file.{Files, Path, Paths}
+
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpMethods.HEAD
 import akka.http.scaladsl.model.{HttpRequest, Uri}
-import akka.http.scaladsl.settings.ConnectionPoolSettings
-import com.typesafe.config.Config
 import edu.sjsu.cmpe256.ghost_towns.Reaper.Reap
 import resource.managed
 
@@ -15,17 +16,34 @@ import scala.io.Source
 import scala.util.{Failure, Success}
 
 object Reaper {
-  final case class Reap(uri: Uri)
+  val path: Path = Paths.get("output/reaper.csv")
+  Files.write(
+    path,
+    "url,isAlive\n".getBytes(),
+//    CREATE_NEW)
+    TRUNCATE_EXISTING)
 
-  def apply(): Behavior[Reap] = Behaviors.receive { (context, message) =>
-    context.log.info(s"Requesting $message")
+  final case class Reap(dnsName: String)
+
+  def apply(): Behavior[Reap] = Behaviors.receive { (context, reap) =>
     implicit val executionContext: ExecutionContextExecutor = context.executionContext
     implicit val actorSystem: ActorSystem[Nothing] = context.system
-    Http(actorSystem).singleRequest(HttpRequest(HEAD, message.uri)).onComplete {
+    val uri = Uri(s"http://${new String(reap.dnsName.getBytes("ISO-8859-1"), "UTF-8")}")
+    Http(actorSystem).singleRequest(HttpRequest(HEAD, uri)).onComplete {
       case Success(httpResponse) =>
-        context.log.info(s"${message.uri} Status Code: ${httpResponse.status.intValue}")
+        context.log.info(s"$uri Status Code: ${httpResponse.status.intValue}")
+        val statusCode = httpResponse.status.intValue.toString
+        Files.write(
+          path,
+          s"${reap.dnsName},${(!(statusCode.startsWith("4") || statusCode.startsWith("5"))).toString}\n".getBytes(),
+          CREATE, APPEND)
         httpResponse.discardEntityBytes()
-      case Failure(_)   => context.log.error(s"${message.uri} Status Code: Failed")
+      case Failure(_) =>
+        context.log.error(s"$uri Status Code: Failed")
+        Files.write(
+          path,
+          s"${reap.dnsName},false\n".getBytes(),
+          CREATE, APPEND)
     }
     Behaviors.same
   }
@@ -33,12 +51,15 @@ object Reaper {
 
 object ReaperApp extends App {
   val reaperActorSystem = ActorSystem(Reaper(), "reaper")
-  val uris =
-    if (false) List("co.jp", "hyipola.com", "tapchitienao.com")
-    else managed(
-      Source.fromFile("/Users/aselvia/Developer/github.com/AndrewSelviaSJSU/cmpe256-ghost-towns/output/vertices/part-00000-fe055b24-eaf6-4ed8-9aaa-946a6521faa5-c000.csv"))
-      .map(_.getLines.drop(1).map(_.split(",").last).toList)
-      .opt
-      .getOrElse(List())
-  uris.foreach(dnsName => reaperActorSystem ! Reap(Uri(s"http://${new String(dnsName.getBytes("ISO-8859-1"), "UTF-8")}")))
+  val fileName = args(0)
+  val uris = managed(Source.fromFile(fileName))
+    .map(_.getLines.drop(1).map(_.split(",")(1)).toList)
+    .opt
+    .getOrElse(List())
+
+  uris
+    .foreach(dnsName => {
+      reaperActorSystem ! Reap(dnsName)
+      Thread.sleep(100)
+    })
 }
